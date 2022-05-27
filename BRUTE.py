cdaa312020308bb36eeb8ad15da36aa28578db29
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-05-27 19:02:45 -0400

    Add files via upload

---
+++ BRUTE.py
@@ -0,0 +1,17 @@
+import time
+import itertools
+import string
+
+def guess_password(real):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    attempts = 0
+    for password_length in range(1, 20):
+        for guess in itertools.product(chars, repeat=password_length):
+            attempts += 1
+            guess = ''.join(guess)
+            if guess == real:
+                return 'the password is {}, found in {} guesses.'.format(guess, attempts)
+            print(guess, attempts)
+		
+
+print(guess_password('Henking21?!'))
